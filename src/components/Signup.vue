8535fc6280d1c879fe8fae15f5d5e1df0f0ea433
stinkymonkeyph <monkeydev.team@gmail.com>
2018-10-21 10:59:40 -0700

    reset build

--- src/components/Signup.vue
+++ src/components/Signup.vue
@@ -1,115 +1,168 @@
 <template>
 	<div style="overflow: auto" class="background-cover">
 		<br />
-		<div class="inner-div md-elevation-1">
+		<div class="inner-div md-elevation-2">
 			<div style="width: 90%; margin: 0 auto;">
 				<br /><br />
 					<h4 class="form-title">Employee Registration Form</h4>
 					<p>Fill up information needed in the form below to sign up as an employee.</p>
 					<br />
-				<b-row>
-					<b-col>
-						<md-field>
-					      <label>* First Name</label>
-					      <md-input v-model="firstName"></md-input>
-    					</md-field>
-					</b-col>
-			        <b-col cols="2">
-			        	<md-field>
-					      <label>Middle Initial</label>
-					      <md-input v-model="middleName"></md-input>
-    					</md-field>
-			        </b-col>
-			        <b-col>
-			        	<md-field>
-					      <label>* Last Name</label>
-					      <md-input v-model="lastName"></md-input>
-    					</md-field>
-			        </b-col>
-				</b-row>
-				<b-row>
-					<b-col>
-						<md-field>
-					      <label>* City</label>
-					      <md-input v-model="city"></md-input>
-	    				</md-field>	
-    				</b-col>
-    				<b-col>
-						<md-field>
-					      <label>* Barangay</label>
-					      <md-input v-model="barangay"></md-input>
-	    				</md-field>	
-    				</b-col>
-				</b-row>
-				<b-row>
-					<b-col>
-						<md-field>
-					      <label>* Date of Birth</label>
-					      <md-input v-model="dateOfBirth"></md-input>
-	    				</md-field>
-					</b-col>
-					<b-col>
-						<md-field>
-					      <label>* Contact No.</label>
-					      <md-input v-model="contactNo" type="number"></md-input>
-	    				</md-field>
-					</b-col> 	
-				</b-row>
-				<b-row>
-					<b-col>
-						<md-field>
-					      <label>* Job</label>
-					      <md-input v-model="job"></md-input>
-	    				</md-field>
-    				</b-col>
-    				<b-col>
-						<md-field>
-					      <label>* Expected Salary</label>
-					      <md-input v-model="expectedSalary"></md-input>
-	    				</md-field>
-    				</b-col>	
-				</b-row>
-				<b-row>
-					<b-col>
-						<md-field>
-					      <label>* Years of Experience</label>
-					      <md-input v-model="job"></md-input>
-	    				</md-field>
-    				</b-col>	
-    				<b-col>
-						<md-field>
-					      <label>* Service Type</label>
-					      <md-input v-model="serviceType"></md-input>
-	    				</md-field>
-    				</b-col>
-				</b-row>
-				<b-row>
-					<b-col>
-						<md-field>
-					      <label>* Skills</label>
-					      <md-input v-model="skills"></md-input>
-	    				</md-field>
-	    				<p style="margin-top: -15px; color: gray;"><i>Please separate your skills with a comma (e.g. Cooking, Sketching, Painting).</i></p>
-    				</b-col>	
-				</b-row>
-				<b-row>
-					<b-col>
-						<md-field>
-					      <label>* Certifications</label>
-					      <md-file v-model="skills"></md-file>
-	    				</md-field>
-	    				<p style="margin-top: -15px; color: gray;"><i>Please upload your certifications as an image file.</i></p>
-    				</b-col>	
-				</b-row>
-				<b-row>
-					<b-col></b-col>
-					<b-col></b-col>
-					<b-col align-v="end">
-						<md-button class="md-raised md-primary button-stable" >
-							Proceed &nbsp; <i class="fa fa-arrow-right" />
-						</md-button>
-					</b-col>
-				</b-row>
+				<div v-show="(step === 1)">
+					<b-row>
+						<b-col>
+							<b><i class="fa fa-user"/>&nbsp;Fillup Personal Informations</b>
+						</b-col>
+					</b-row>
+					<b-row>
+						<b-col>
+							<md-field>
+						      <label>* First Name</label>
+						      <md-input v-model="firstName"></md-input>
+	    					</md-field>
+						</b-col>
+				        <b-col cols="2">
+				        	<md-field>
+						      <label>Middle Initial</label>
+						      <md-input v-model="middleName"></md-input>
+	    					</md-field>
+				        </b-col>
+				        <b-col>
+				        	<md-field>
+						      <label>* Last Name</label>
+						      <md-input v-model="lastName"></md-input>
+	    					</md-field>
+				        </b-col>
+					</b-row>
+					<b-row>
+						<b-col>
+							<md-field>
+						      <label>* City</label>
+						      <md-input v-model="city"></md-input>
+		    				</md-field>	
+	    				</b-col>
+	    				<b-col>
+							<md-field>
+						      <label>* Barangay</label>
+						      <md-input v-model="barangay"></md-input>
+		    				</md-field>	
+	    				</b-col>
+					</b-row>
+					<b-row>
+						<b-col>
+							<md-field>
+						      <label>* Date of Birth</label>
+						      <md-input v-model="dateOfBirth"></md-input>
+		    				</md-field>
+						</b-col>
+						<b-col>
+							<md-field>
+						      <label>* Contact No.</label>
+						      <md-input v-model="contactNo" type="number"></md-input>
+		    				</md-field>
+						</b-col> 	
+					</b-row>
+					<b-row>
+						<b-col>
+							<md-field>
+						      <label>* Job</label>
+						      <md-input v-model="job"></md-input>
+		    				</md-field>
+	    				</b-col>
+	    				<b-col>
+							<md-field>
+						      <label>* Expected Salary</label>
+						      <md-input v-model="expectedSalary"></md-input>
+		    				</md-field>
+	    				</b-col>	
+					</b-row>
+					<b-row>
+						<b-col>
+							<md-field>
+						      <label>* Years of Experience</label>
+						      <md-input v-model="yearsOfExperience"></md-input>
+		    				</md-field>
+	    				</b-col>	
+	    				<b-col>
+							<md-field>
+					          <label for="movie">Service Type</label>
+					          <md-select v-model="service">
+					            <md-option value="Home">Home Service</md-option>
+					            <md-option value="Shop">Shop Service</md-option>
+					            <md-option value="HomeShop">Home and Shop Service</md-option>
+					          </md-select>
+					        </md-field>
+	    				</b-col>
+					</b-row>
+					<b-row>
+						<b-col>
+							<md-field>
+						      <label>* Skills</label>
+						      <md-input v-model="skills"></md-input>
+		    				</md-field>
+		    				<p style="margin-top: -15px; color: gray;"><i>Please separate your skills with a comma (e.g. Cooking, Sketching, Painting).</i></p>
+	    				</b-col>	
+					</b-row>
+					<b-row>
+						<b-col>
+							<md-field>
+						      <label>* Certifications</label>
+						      <md-file v-model="skills"></md-file>
+		    				</md-field>
+		    				<p style="margin-top: -15px; color: gray;"><i>Please upload your certifications as an image file.</i></p>
+	    				</b-col>	
+					</b-row>
+					<b-row>
+						<b-col><b style="font-size:16px;">Step 1 of 2</b></b-col>
+						<b-col></b-col>
+						<b-col align-v="end">
+							<md-button class="md-raised md-primary button-stable" v-on:click="proceed">
+								Proceed &nbsp; <i class="fa fa-arrow-right" />
+							</md-button>
+						</b-col>
+					</b-row>
+				</div>
+				<div v-show="(step === 2)">
+					<b-row>
+						<b-col>
+							<b><i class="fa fa-lock"/>&nbsp;Setup Account Information</b>
+						</b-col>
+					</b-row>
+					<b-row>
+						<b-col>
+							<md-field>
+						      <label>* Email Address</label>
+						      <md-input v-model="email"></md-input>
+	    					</md-field>
+						</b-col>
+					</b-row>
+					<b-row>
+						<b-col>
+							<md-field>
+						      <label>* Password</label>
+						      <md-input v-model="password" type="password"></md-input>
+	    					</md-field>
+						</b-col>
+						<b-col>
+							<md-field>
+						      <label>* Confirm Password</label>
+						      <md-input v-model="confirmPassword" type="password"></md-input>
+	    					</md-field>
+						</b-col>
+					</b-row>
+					<b-row>
+						<b-col><b style="font-size:16px;">Step 2 of 2</b></b-col>
+						<b-col>
+							<md-button class="md-raised md-primary button-stable" v-on:click="previous" style="background-color: #27ae60;">
+								<i class="fa fa-arrow-left" /> &nbsp; Previous 
+							</md-button>
+						</b-col>
+						<b-col align-v="end">
+							<md-button class="md-raised md-primary button-stable">
+								Submit &nbsp; <i class="fa fa-paper-plane" />
+							</md-button>
+						</b-col>
+					</b-row>
+				</div>
 			</div>
 			<br /><br />
 		</div>
@@ -126,26 +179,34 @@
 				middleName: '',
 				lastName: '',
 				email: '',
+				username: '',
 				password: '',
+				confirmPassword: '',
 				address: '',
 				city: '',
 				barangay: '',
 				dateOfBirth: '',
 				contactNo: '',
 				job: '',
+				yearsOfExperience: '',
 				skills: '',
 				step: 1
 			}
 		},
 		methods: {
-				
+			proceed: function () {
+				this.step += 1
+			},
+			previous: function () {
+				this.step -= 1
+			}
 		}
 	}
 </script>
 
 <style scoped>
 .inner-div {
-	min-height: 500px;
+	min-height: 300px;
 	max-height: 0 auto;
 	width: 60%;
 	margin: 0 auto;
